8583a8c0a5b66885684ba33e38415492dfc5af37
Ahmed Trabelsi <narigua@gmail.com>
2013-03-13 12:20:57 +0100

    Add Backoffice with home view and menu

---
+++ Controller/BackOfficeController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace MP\Bundle\CatalogBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+/**
+ * @Route("/admin")
+ */
+class BackOfficeController extends Controller
+{
+    /**
+     * @Route("/", name="admin_home")
+     * @Template()
+     */
+    public function indexAction()
+    {
+        return array();
+    }
+}
